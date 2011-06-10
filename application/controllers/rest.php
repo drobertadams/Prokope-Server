63f2569d17e4103049c80db0ae1df703c927306b
Robert Adams <d.robert.adams@gmail.com>
2011-06-10 09:10:09 -0400

    Added a big list of bogus XML data for testing the UI.

--- application/controllers/rest.php
+++ application/controllers/rest.php
@@ -19,4 +19,68 @@ class rest extends CI_Controller
 		$this->load->model("Author_model");
 		$this->load->view('documents_xml');
 	}
+
+	/** Dummy list of lots of data. */
+	public function big()
+	{
+		$this->output->set_header("Content-type: application/xml; charset=UTF-8");
+		$this->output->set_output(<<<EOT
+		<prokope>
+			<author name="Cicero" icon="url">
+				<work name="Work 1" url="url" />
+				<work name="Work 2" url="url" />
+				<work name="Work 3" url="url" />
+			</author>
+			<author name="Martial" icon="url">
+				<work name="Work 0" url="url" />
+				<work name="Work 1" url="url" />
+				<work name="Work 2" url="url" />
+				<work name="Work 3" url="url" />
+				<work name="Work 4" url="url" />
+				<work name="Work 5" url="url" />
+				<work name="Work 6" url="url" />
+				<work name="Work 7" url="url" />
+				<work name="Work 8" url="url" />
+				<work name="Work 9" url="url" />
+				<work name="Work 10" url="url" />
+				<work name="Work 11" url="url" />
+				<work name="Work 12" url="url" />
+				<work name="Work 13" url="url" />
+				<work name="Work 14" url="url" />
+				<work name="Work 15" url="url" />
+				<work name="Work 16" url="url" />
+				<work name="Work 17" url="url" />
+				<work name="Work 18" url="url" />
+				<work name="Work 19" url="url" />
+				<work name="Work 20" url="url" />
+				<work name="Work 21" url="url" />
+				<work name="Work 22" url="url" />
+				<work name="Work 23" url="url" />
+				<work name="Work 24" url="url" />
+				<work name="Work 25" url="url" />
+				<work name="Work 26" url="url" />
+				<work name="Work 27" url="url" />
+				<work name="Work 28" url="url" />
+				<work name="Work 29" url="url" />
+			</author>
+			<author name="Agricola" icon="url">
+				<work name="Work 1" url="url" />
+				<work name="Work 2" url="url" />
+				<work name="Work 3" url="url" />
+			</author>
+			<author name="Caeser" icon="url">
+				<work name="Work 1" url="url" />
+				<work name="Work 2" url="url" />
+				<work name="Work 3" url="url" />
+			</author>
+			<author name="Pliny" icon="url">
+				<work name="Work 1" url="url" />
+				<work name="Work 2" url="url" />
+				<work name="Work 3" url="url" />
+			</author>
+		</prokope>
+EOT
+		);
+
+	}
 }
