6dc6a3978ac954bc4a8cde754ea7f8ee81dc61ac
Robert Adams <d.robert.adams@gmail.com>
2011-07-08 22:00:43 -0400

    Added support for embedded images in the commentary.

--- application/helpers/my_document_helper.php
+++ application/helpers/my_document_helper.php
@@ -3,9 +3,44 @@
  * My custom document helper.
  */
 
+if ( ! function_exists('__inject_images')) {
+	/* Called for each <img> comment element. Injects the img element into the appropriate spot
+	 * in the document, and returns "", which will remove the img element from the commentary.
+	 * $matches[0] will be the entire img elemenet that matched the RE, $matches[1]... will be the
+	 * word reference (e.g., 10.1.4.2).
+	 */
+	function __inject_images($matches)
+	{
+		$CI =& get_instance();
+
+		$img = $matches[0];	// the entire img element 
+
+		/* Convert the img element into an clickable <a href>. */
+		if ( preg_match('/<img.+src="([^"]+)"/ims', $img, $m) ) {
+			$src = $m[1]; // the image src URI
+			$img = "<a href=\"$src\">" . $img . "</a>";
+		}
+
+		/* Now insert $img into the document. */
+		$ref = $matches[1]; // the ref attribute of the img element
+		$pos = strpos($CI->Document_model->content, $ref); // find the ref in the document
+		if ($pos > 0) {
+			/* The ref should appear in an <a href="REF"...> element. $pos will refer
+			 * to where that REF appears. Scan forward to the next </a> element.
+			 */
+			$close_a = stripos($CI->Document_model->content, "</a>", $pos);
+			$close_a += 4; // move over the "</a>" characters.
+			/* Now insert the image. */
+			$CI->Document_model->content = substr_replace($CI->Document_model->content, $img, $close_a, 0);
+		}
+
+		return "";
+	}
+}
+
 // ------------------------------------------------------------------------
 /** Utility function to fetch a document and all associated components. 
- * Returns a array of {document, comment, vocabulary, sidebar} containing
+ * Returns an array of {document, comment, vocabulary, sidebar} containing
  * models.
  * @id is the id of the document.
  */
@@ -19,7 +54,7 @@ if ( ! function_exists('get_document_components'))
 		$CI->load->model('Document_model');
 		$CI->Document_model->get($id);
 
-		// ### Massage the document to make it suitable for display. ###
+		// Massage the document to make it suitable for display. ###
         // Force a line break after each line.
         $CI->Document_model->content = str_replace("</l>", "</l><br/>", $CI->Document_model->content);
         // Make sure that "indent"ed lines are indented. &#160; is the XML version of &nbsp.
@@ -40,6 +75,9 @@ if ( ! function_exists('get_document_components'))
         // Convert "comment" to "li" (list items).
 		$CI->Comment_model->content = str_replace('<comment ', '<li ', $CI->Comment_model->content);
 		$CI->Comment_model->content = str_replace("</comment>", "</li>", $CI->Comment_model->content);
+		// Find and remove all <img> comments and inject them into the document.
+		$CI->Comment_model->content = preg_replace_callback('/<img\s+ref="([0-9.]+)"\s+src="[^"]+"\s*\/>/ims', 
+			"__inject_images", $CI->Comment_model->content);
 
 		// Fetch the associated vocabulary.
 		$CI->load->model('Vocabulary_model');
